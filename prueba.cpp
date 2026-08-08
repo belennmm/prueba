bb27450480840351cccbdcfd2071ba832ed67fb9
Belen Monterroso <belenmonterroso@gmail.com>
2026-08-07 18:00:00 -0600

    prueba de commit 2 de antes

--- prueba.cpp
+++ prueba.cpp
@@ -1,1 +1,12 @@
-# AAAAAAAAAAAAAAAAA
+#include <iostream>
+#include <string>
+
+int main() {
+    // 1. Crear una variable de tipo string con el texto
+    std::string mensaje = "¡Hola, este es un string!";
+
+    // 2. Mostrar el string en la pantalla
+    std::cout << mensaje << std::endl;
+
+    return 0;
+}
